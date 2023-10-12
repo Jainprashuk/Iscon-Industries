1e464d4cafad0077068e311b47056a4326ac6d2a
Prashuk Jain <29jainprashuk@gmail.com>
2023-10-12 14:02:27 +0530

    files uploaded

---
+++ app/about/page.tsx
@@ -0,0 +1,34 @@
+
+
+export default function about() {
+  return (
+    <div className="mt-10 p-5 mx-auto sm:p-10 md:p-16 dark:bg-gray-800 dark:text-gray-100">
+	<div className="flex flex-col max-w-5xl mx-auto overflow-hidden rounded">
+		<img src="https://source.unsplash.com/random/480x360" alt="" className="w-full h-60 sm:h-96 bg-gray-500" />
+		<div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md bg-gray-400">
+			<div className="space-y-2">
+				<a rel="noopener noreferrer" href="#" className="text-center inline-block text-2xl font-semibold sm:text-3xl text-gray-900">Unveiling Iscon Industries: Crafting Outdoor Elegance</a>
+				<p className="text-xs dark:text-gray-400">~ By
+					<a rel="noopener noreferrer" href="#" className="text-xs hover:underline">  Ashish Jain</a>
+				</p>
+			</div>
+			<div className="dark:text-gray-100">
+				<p>Iscon Industries, based in Sanawad, MP, is a trusted name in the pavers and tiles industry with a decade of unwavering expertise. With a portfolio of over 50 successful projects, we've earned a reputation for excellence in crafting high-quality, durable pavers that transform outdoor spaces. Our commitment to innovation and design has made us a preferred choice for enhancing landscapes. Explore our range and experience the Iscon difference.</p>
+                <br />
+                <p>At Iscon Industries, we take pride in our unwavering commitment to excellence, ensuring that our pavers not only meet but exceed expectations. Quality assurance is at the heart of our craftsmanship, guaranteeing that every product we create boasts both lasting durability and exceptional aesthetic appeal.
+<br /><br />
+Backed by years of industry knowledge, our team of experts employs precision and innovation in the art of paver crafting. We understand that each project is unique, and our expertise allows us to tailor our products to your specific needs.
+<br /><br />
+One of our core principles is offering cost-effective solutions without ever compromising on quality. We firmly believe that your projects should be both affordable and remarkable, and we're dedicated to making that vision a reality.
+<br /><br />
+But don't just take our word for it – our satisfied clients consistently share their positive experiences, testifying to the outstanding products and exceptional service they've received from Iscon Industries.
+<br /><br />
+Our versatile pavers find their place in a wide range of applications, from enhancing outdoor spaces to creating stunning driveways and walkways. With style and functionality, our products seamlessly adapt to your project's needs.
+<br /><br />
+We understand that successful installation is as crucial as product quality. That's why we offer comprehensive installation support, including expert guidance from our factory, custom plans tailored to your project, recommended installers, and dedicated support throughout the installation process. With Iscon Industries, you can embark on your project with confidence and enjoy a worry-free tile installation experience.</p>
+			</div>
+		</div>
+	</div>
+</div>
+  )
+}
---
+++ components/zigzag.tsx
@@ -0,0 +1,80 @@
+import Image from 'next/image'
+
+import FeatImage01 from '@/public/images/features-03-image-01.png'
+import FeatImage02 from '@/public/images/features-03-image-02.png'
+import FeatImage03 from '@/public/images/features-03-image-03.png'
+
+export default function Zigzag() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            {/* <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Reach goals that matter</div> */}
+            <h1 className="h2 mb-4">About Us</h1>
+            <p className="text-xl text-gray-400">All About Who We Are , What We Offer And How We work ....</p>
+          </div>
+
+          {/* Items */}
+          <div className="grid gap-20">
+
+            {/* 1st item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
+                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage01} width={540} height={405} alt="Features 01" />
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">Iscon - Industries</div>
+                  <h3 className="h3 mb-3">Who We Are ?</h3>
+                  <p className="text-xl text-gray-400 mb-4">Iscon Industries, based in Sanawad, MP, is a trusted name in the pavers and tiles industry with a decade of unwavering expertise. With a portfolio of over 50 successful projects, we've earned a reputation for excellence in crafting high-quality, durable pavers that transform outdoor spaces. Our commitment to innovation and design has made us a preferred choice for enhancing landscapes. Explore our range and experience the Iscon difference. <span className='text-blue-600'><a href="/about">Read More...</a></span> </p>
+                  
+                </div>
+              </div>
+            </div>
+
+            {/* 2nd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
+                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage02} width={540} height={405} alt="Features 02" />
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">Iscon - industries</div>
+                  <h3 className="h3 mb-3">Our Products</h3>
+                  <p className="text-xl text-gray-400 mb-4">Explore our extensive product selection, boasting more than 15 distinct varieties, each showcasing a blend of captivating color combinations. Iscon Industries offers an array of pavers that redefine outdoor aesthetics and elevate your landscape design to new heights. <span className='text-blue-600'><a href="">Read More...</a></span></p>
+                  
+                </div>
+              </div>
+            </div>
+
+            {/* 3rd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
+                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage03} width={540} height={405} alt="Features 03" />
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">Iscon - industries</div>
+                  <h3 className="h3 mb-3">Our Works till Now..</h3>
+                  <p className="text-xl text-gray-400 mb-4">Over a span of time, Iscon Industries has proudly served 50+ clients, delivering excellence in over 10 major projects. Our commitment to quality and customer satisfaction shines through our track record.  <span className='text-blue-600'><a href="">Read More...</a></span></p>
+                  
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
